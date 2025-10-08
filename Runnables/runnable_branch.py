39bcf340c98ab2c9625cb63ce1cdae342003eead
saivasanth528 <vasisai128@gmail.com>
2025-10-08 23:26:07 +0530

    - Implemented the different types of runnable concepts

---
+++ Runnables/runnable_branch.py
@@ -0,0 +1,36 @@
+from langchain_core.runnables import RunnablePassthrough
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableBranch
+
+load_dotenv()
+
+prompt1 = PromptTemplate(
+    template = 'Write a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template = 'Summarize the following text \n {text}',
+    input_variables=['text']
+)
+
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+report_gen_chain = RunnableSequence(prompt1, model, parser)
+
+branch_chain = RunnableBranch(
+    (lambda x: len(x.split()) > 300, RunnableSequence(prompt2, model, parser)),
+            RunnablePassthrough()
+)
+
+
+
+final_chain = RunnableSequence(report_gen_chain, branch_chain)
+
+print(final_chain.invoke({'topic': 'Russia Vs Ukraine'}))
---
+++ Runnables/runnable_passthrough.py
@@ -0,0 +1,42 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from dotenv import load_dotenv
+from langchain.schema.runnable import RunnableSequence, RunnableParallel, RunnablePassthrough
+
+load_dotenv()
+
+passthrough = RunnablePassthrough()
+print(passthrough.invoke(28))
+
+
+
+prompt1 = PromptTemplate(
+    template = 'Write a joke about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatOpenAI()
+
+parser = StrOutputParser()
+
+
+prompt2 = PromptTemplate(
+    template = 'Explain the following joke - {text}',
+    input_variables=['text']
+)
+
+chain = RunnableSequence(prompt1, model, parser, prompt2, model, parser)
+print(chain.invoke({'topic': 'AI'}))
+
+joke_gen_chain = RunnableSequence(prompt1, model, parser)
+
+parallel_chain = RunnableParallel({
+    'joke': RunnablePassthrough(),
+    'explanation': RunnableSequence(prompt2, model, parser)
+})
+
+final_chain = RunnableSequence(joke_gen_chain, parallel_chain)
+
+print(final_chain.invoke({'topic': 'Cricket'}))
+
